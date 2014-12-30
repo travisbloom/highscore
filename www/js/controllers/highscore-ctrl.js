2414fee55713ad6f66f2be99cb3a35d804a9629c
travisbloom <travisbloom@gmail.com>
2014-12-30 16:17:45 -0500

    basics for icon modal

--- www/js/controllers/highscore-ctrl.js
+++ www/js/controllers/highscore-ctrl.js
@@ -1,19 +1,29 @@
 angular.module('starter')
-  .controller('HighScoreCtrl', function($scope, $stateParams, highScoreFactory) {
-    $scope.collapsed = {
-      configItem: true
+  .controller('HighScoreCtrl', function($scope, $stateParams, highScoreFactory, dataModelFactory, $ionicModal) {
+    //default config for collapsable elemnts
+    $scope.config = {
+      collapsed: true,
+      options: {
+        type: dataModelFactory.config.type
+      }
     };
+    //pull the item from the url params
     $scope.item = highScoreFactory.getScores()[$stateParams.highscoreindex];
+    //set newScore to track if it differs from currentScore
     $scope.newScore = $scope.item.currentScore;
+    //graph config options
     $scope.options = {
       axes: {
         x: {
           key: 'date',
+          //treat x axis as date
           type: 'date',
+          //apply relative timing function to labels
           labelFunction: function(date) {
             var relTime = moment(date).fromNow();
             return relTime;
           },
+          //number of x axis ticks
           ticks: 2
         }
       },
@@ -25,10 +35,26 @@ angular.module('starter')
       }},
       drawLegend: false
     };
+    //icon modal configuration/injection
+    $ionicModal.fromTemplateUrl('templates/modal-icons.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    $scope.icons = dataModelFactory.config.icon;
+    //increment the item then refresh newScore
+    $scope.increment = function (direction) {
+      $scope.item.increment(direction);
+      $scope.newScore = $scope.item.currentScore;
+    };
     $scope.configureSettings = function () {
-      $scope.collapsed.configItem = !$scope.collapsed.configItem;
-      if ($scope.collapsed.configItem) {
+      $scope.config.collapsed = !$scope.config.collapsed;
+      if ($scope.config.collapsed) {
         $scope.item.saveObj($scope.item.config);
       }
+    };
+    $scope.selectIcon = function (icon) {
+
     }
   });
---
+++ www/js/factories/data-model-factory.js
@@ -0,0 +1,52 @@
+angular.module('starter')
+  .factory('dataModelFactory', function() {
+    return {
+        id: 'string',
+        config: {
+          name: 'string',
+          icon: [
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd',
+            'ion-social-usd'
+          ],
+          color: '#26335b',
+          type: ['num', 'usd']
+        },
+        highScore: 4,
+        currentScore: 2,
+        incrementValue: 1,
+        history: [
+          {
+            date: 'time',
+            score: 2
+          }
+        ]
+    };
+  });
