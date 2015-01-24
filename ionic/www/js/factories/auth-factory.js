fbd9d5a7e4d0a33d2ae6d48e6ec72fa494931294
travisbloom <travisbloom@gmail.com>
2015-01-24 14:49:39 -0500

    clean code

--- ionic/www/js/factories/auth-factory.js
+++ ionic/www/js/factories/auth-factory.js
@@ -1,12 +1,7 @@
 angular.module('starter')
   .factory('authFactory', function($auth, $q, localFactory) {
-    var authIndexes = {}, providers = localFactory.appData.userData.providers;
+    var  providers = localFactory.appData.userData.providers;
     return {
-      addAuthIndex: function(scoreObj) {
-        var provider = scoreObj.apiInfo.provider;
-        if (!authIndexes[provider])
-        authIndexes[scoreObj.id] = scoreObj.apiInfo
-      },
       getAuth: function (provider) {
         var deferred = $q.defer();
         //if the provider doesn't exist, authenticate the user
@@ -18,12 +13,6 @@ angular.module('starter')
         }
         deferred.resolve(providers[provider].access_token);
         return deferred.promise;
-      },
-      addAuth: function (provider) {
-        return $auth.authenticate(provider).then(function (response) {
-          providers[provider] = response.data;
-          return response.data;
-        });
       }
     };
   });
