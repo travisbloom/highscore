dabe658950ffe9b08648f9b127774f820510b7e8
travisbloom <travisbloom@gmail.com>
2015-02-01 13:22:33 -0500

    separated api server from app repo

--- apiserver/routes/standard-response.js
+++
@@ -1,7 +0,0 @@
-exports.error = function(res, errObj) {
-  res.status(errObj.status || 500).json({
-    userMessage: errObj.userMessage || 'There was an error with your request, please try again later',
-    internalMessage: errObj.internalMessage || this.userMessage,
-    details: errObj.details || null
-})
-};
